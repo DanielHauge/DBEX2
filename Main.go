296c49a1b478101de0137da187e120f8423d6f25
DanielHauge <Animcuil@gmail.com>
2018-02-10 23:24:31 +0100

    Last few fixes

--- Main.go
+++ Main.go
@@ -19,7 +19,7 @@ import (
 func main() {
 
 
-	log.Println("GoApplication!, you can now go to "+GetMyIP()+":9191 and interact from there or type in one of the CLI commands here")
+	log.Println("GoApplication READY!!! you can now go to "+getMyIP()+":9191 and interact from there or type in one of the CLI commands here")
 	/*
 	//println(UserCount())
 	//test := MostLinks()
